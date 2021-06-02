1d2061125b30281cfbbe0aef932274dd4dbc334d
pablo <pablodelfantexp@gmail.com>
2021-06-02 16:08:00 -0300

    @delete resize lines code in config/plugins.js -> (problem at resize image with particular library), @delete env variables in database config, and use .env file and enviroment variables en heroku, alone USER_NAME and USER_PASSWORD

--- config/database.js
+++ config/database.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => ({
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'strapi-db'),
-        username: env('DATABASE_USERNAME', 'lapabla'),
-        password: env('DATABASE_PASSWORD', 'lapabla'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
